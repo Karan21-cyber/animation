bdcd69288ef4d38ae9135e35dfa17d2233d3c32e
Karan Chaudhary <tharukaran930@gmail.com>
2023-10-04 19:56:53 +0545

    changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import { MouseEffect } from "@/mouse/MouseEffect";
 
 export const Footer = () => {
   const [cursorVariant, setCursorVariant] = useState<any>("default");
-  const textEnter = () => setCursorVariant("text");
+  const textEnter = () => setCursorVariant("footer");
   const textLeave = () => setCursorVariant("default");
   return (
     <footer className="h-[30vh] relative flex justify-between items-center px-8 py-8 bg-slate-500">
@@ -15,7 +15,7 @@ export const Footer = () => {
         textLeave={textLeave}
         className="title pl-3 text-3xl text-white w-[100px]"
       />
-      <MouseEffect cursorVariant={cursorVariant} />
+      {/* <MouseEffect cursorVariant={cursorVariant} /> */}
     </footer>
   );
 };
